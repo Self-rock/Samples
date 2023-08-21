26c8245f7201639b4d6bff7b648f9b4f38330f7e
Self-rock <selfrock@yandex.ru>
2023-08-21 15:48:23 +0300

    map ++

--- map.cpp
+++ map.cpp
@@ -2,15 +2,18 @@
 #include <map>
 #include <string>
 using namespace std;
+
 //вывод map в консоль
 	void outmap (map<std::string, int> m){
 	std::map<std::string, int>::iterator it = m.begin();
 	while (it != m.end())
   {
-    std::cout << "Key: " << it->first << ", Value: " << it->second << std::endl;
+    cout << "Key: " << it->first << ", Value: " << it->second << endl;
     ++it;
   }
+  std::cout <<endl;
 }
+
 int main()
 {
     //map - словарь создание с заполнением - ключ стринг, значение int
