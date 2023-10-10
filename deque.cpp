73b01600b7791453f051d6f4ea67a13916708934
Self-rock <selfrock@yandex.ru>
2023-10-10 11:03:35 +0300

    deque

---
+++ deque.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <deque>
+#include <vector>
+using namespace std;
+
+template <typename T>
+void dqprint(T deq){
+    while (!deq.empty())
+    {
+        cout << deq.front()<<" ";
+        deq.pop_front();
+        //cout << deq.back();
+        //deq.pop_back();
+
+    }
+    cout << endl;
+}
+
+int main(int argc, char const *argv[])
+{
+    deque<int> dq;
+    deque<int> dq2;
+    
+    dq.push_back(2);
+    dq.emplace_back(2);
+    dq.push_front(4);
+    dq.emplace_front(3);
+    dqprint(dq);
+
+    dq.push_back(2);
+    cout << "size deque "<< dq.size()<<endl;
+    dq.clear();
+    cout << "size deque after dq.clear() "<< dq.size()<<endl;
+    
+    dq2.push_back(28);   
+    dq.swap(dq2);
+    cout << "dq after dq.swap(dq2) "<<endl;
+    dqprint(dq);
+    
+    return 0;
+}
