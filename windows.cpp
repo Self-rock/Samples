75bd22224889a5640fb374e9e327e89974c15ff9
Self-rock <selfrock@yandex.ru>
2023-12-03 18:32:34 +0300

    windows

---
+++ windows.cpp
@@ -0,0 +1,12 @@
+#include <windows.h> // заголовочный файл, содержащий функции API
+ 
+// Основная функция - аналог int main() в консольном приложении:
+int WINAPI WinMain(HINSTANCE hInstance, // дескриптор экземпляра приложения
+                   HINSTANCE hPrevInstance, // в Win32 не используется
+                   LPSTR lpCmdLine, // нужен для запуска окна в режиме командной строки
+                   int nCmdShow) // режим отображения окна
+{
+        // Функция вывода окна с кнопкой "ОК" на экран (о параметрах позже)
+        MessageBox(NULL, "Hello, world!!!", "Windows procedure", MB_OK);
+        return 0; // возвращаем значение функции
+}
