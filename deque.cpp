2777ba0bf29b3658f3aa2aea819aecb082eafea6
Self-rock <selfrock@yandex.ru>
2024-03-01 21:37:52 +0300

    deque

--- deque.cpp
+++ deque.cpp
@@ -25,6 +25,18 @@ int main(int argc, char const *argv[])
     dq.emplace_back(2);
     dq.push_front(4);
     dq.emplace_front(3);
+    cout <<"dq[1]="<<dq[1]<< endl;//доступ к элементу с индексом 1(если индекс некорректен, возможно неопределенное поведение)
+    cout <<"dq.at(0)="<<dq.at(0)<< endl;//доступ к элементу с индексом 0(при обращении по некорректному индексу она генерирует исключение out_of_range)
+    try{
+        dq.at(1000) = 1;//вводим неправельный индекс
+    }
+    catch (const out_of_range&)
+    {
+        cout << "Incorrect index" << std::endl;//ловим исключение на неправельный индекс
+    }
+    cout <<"dq.front()="<<dq.front()<< endl;//доступ к элементу front
+    cout <<"dq.back()="<<dq.back()<< endl;//доступ к элементу back
+    
     dqprint(dq);
 
     dq.push_back(2);
