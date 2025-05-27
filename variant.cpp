f40e7c125b5bc75738d94b83d378bf41abd6bb21
Self-rock <selfrock@yandex.ru>
2025-05-27 20:29:55 +0300

    variant

---
+++ variant.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <variant>
+
+int main(int argc, char const *argv[])
+{
+    std::variant <int, double> v1;
+    v1 = 1;//активируем int значение
+    std::cout << "v1.index() = " << v1.index() <<std::endl;
+    std::cout << "std::get<0>(v1) = " << std::get<0>(v1) <<std::endl;
+
+    v1 = 3.14;//активируем double
+    std::cout << "v1.index() = " << v1.index() <<std::endl;
+    std::cout << "std::get<1>(v1) = " << std::get<1>(v1) <<std::endl;
+    std::cout <<std::endl;
+
+    
+
+    return 0;
+}
