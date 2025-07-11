a4a26c15623691478274bf8f019a299a324d8e2d
Self-rock <selfrock@yandex.ru>
2025-07-11 15:18:25 +0300

    clearscreen

---
+++ clearscreen.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+
+void clear_screen()
+{
+#ifdef WINDOWS
+    std::system("cls");
+#else
+    // Assume POSIX
+    std::system ("clear");
+#endif
+}
+int main(int argc, char const *argv[])
+{
+    std::cout<<"Hello";
+clear_screen();
+    return 0;
+}
