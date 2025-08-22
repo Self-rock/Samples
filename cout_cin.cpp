5eda1542278eee2bd137a4dd7bd33f5c833cd703
Self-rock <selfrock@yandex.ru>
2025-08-22 14:49:11 +0300

    cout_extern_C

---
+++ cout_cin.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <iomanip>
+
+int main(int argc, char const *argv[])
+{
+    //вывод с заданной сточностью знаков после запятой
+    double a =4.5465465654654;
+    std::cout<<"a="<< std::fixed<<std::setprecision(2)<<a<<"\n";
+/*setprecision устанавливает «количество цифр», 
+которое в зависимости от состояния std::fixed может быть либо «значащими цифрами», 
+либо «цифрами после запятой», либо «цифрами после шестнадцатеричного разряда»*/
+
+
+    double answer=5.9999;
+    double answer2=55.1000;
+    std::cout<<std::setprecision(2)<<std::fixed;
+    std::cout <<"answer="<<answer << std::endl;
+std::cout<<std::setprecision(0)<<std::fixed;//сбрасываем точность после запятой
+    std::cout <<"answer2="<<answer2 << std::endl;
+
+    /*
+    не проверено
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
+    */
+   
+
+    return 0;
+}
